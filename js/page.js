e0827b9f742a05a85432275065dddd6e2b7710dc
Albert Huang <alberthdev@users.noreply.github.com>
2017-02-17 18:49:59 -0500

    Modify hover JS to only enable when page is wide enough

--- js/page.js
+++ js/page.js
@@ -1,6 +1,10 @@
 //Add Hover effect to menus
 $('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(100);
+    if ($(window).width() >= 768) {
+        $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(100);
+    }
 }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(100);
+    if ($(window).width() >= 768) {
+        $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(100);
+    }
 });
