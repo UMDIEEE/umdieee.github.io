181a670fe5fd8066bd06386a399edf6ca123ba4d
Albert Huang <alberthdev@users.noreply.github.com>
2017-02-17 19:23:35 -0500

    Adjust mobile styles + fix responsive mobile hover

--- js/page.js
+++ js/page.js
@@ -1,10 +1,14 @@
-//Add Hover effect to menus
+// Add Hover effect to menus
 $('ul.nav li.dropdown').hover(function() {
     if ($(window).width() >= 768) {
         $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(100);
     }
 }, function() {
     if ($(window).width() >= 768) {
-        $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(100);
+        $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(100, function() {
+                /* Unset "display: none;" */
+                this.style.display = "";
+            }
+        );
     }
 });
