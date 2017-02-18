df9256d0ed6799e9c568a9fc049baf09cb9f4958
Albert Huang <alberthdev@users.noreply.github.com>
2017-02-18 00:15:02 -0500

    Fix up behavior for hover

--- js/page.js
+++ js/page.js
@@ -12,3 +12,24 @@ $('ul.nav li.dropdown').hover(function() {
         );
     }
 });
+
+function umdieee_toggleAdjust() {
+    var menus = $('ul.nav li.dropdown .dropdown-toggle');
+    if ($(window).width() >= 768) {
+        console.log("attempting to remove data-toggle attr");
+        menus.removeAttr("data-toggle");
+        menus.attr('aria-expanded', 'false');
+        menus.parent().removeClass('open');
+        menus.find('.dropdown-menu').stop(true, true).delay(0).fadeOut(100, function() {
+                /* Unset "display: none;" */
+                this.style.display = "";
+            }
+        );
+    } else {
+        console.log("adding data-toggle attr");
+        menus.attr("data-toggle", "dropdown");
+    }
+}
+
+$(window).resize(umdieee_toggleAdjust);
+$(document).ready(umdieee_toggleAdjust);
